b9b8f81e65fc1cd7f6081920ddfd85a25e80a976
PeterChauYEG <peter.chau.esq@gmail.com>
2018-07-06 20:04:47 -0600

    cleaned up code

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as tf from '@tensorflow/tfjs'
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -10,84 +11,119 @@ class App extends Component {
       model: null
     }
 
-    this.reconstructImage = this.reconstructImage.bind(this)
+    this.styleImage = this.styleImage.bind(this)
   }
 
   componentWillMount() {
-    const loadModel = async () => await tf.loadModel('http://localhost:3000/layersModel/model.json')
+    // load the model
+    this.loadModel('/layersModel/model.json')
+  }
+
+  componentDidMount() {
+    // draw example image
+    this.updateCanvas('/example-104.png')
+  }
+
+  // loads the model from a file then stores the ref to state
+  loadModel(modelPath) {
+    const loadModel = async () => await tf.loadModel(modelPath)
 
     loadModel().then(model => this.setState({ model }))
   }
 
-  componentDidMount() {
-    this.updateCanvas()
-  }
-
-  updateCanvas() {
+  // draw an image on to a canvas using it's path
+  updateCanvas(imagePath) {
     // grab canvas context
     const ctx = this.refs.exampleCanvas.getContext('2d')
 
     // create image
-    var exampleImg = new Image()
+    var image = new Image()
 
     // bind an image to it
-    exampleImg.src = 'http://localhost:3000/example-104.png'
+    image.src = imagePath
 
-    exampleImg.onload = () => {
+    // attach a cb for when the image loads
+    image.onload = () => {
       // draw the image onto the canvas
-      ctx.drawImage(exampleImg, 0, 0)
+      ctx.drawImage(image, 0, 0)
 
+      // grabs image data from a canvas
       this.getImageData(ctx)
     }
   }
 
+  // grabs image data from a canvas using it's context
   getImageData (ctx) {
     var imageData = ctx.getImageData(0, 0, 104, 104)
 
     this.setState({ imageData })
   }
 
-  reconstructImage () {
+  // preprocess the image so that it's in the format we expect
+  preprocessImage(imageData) {
+    // convert the image data into a tf tensor (3D)
+    let processedTensor = tf.fromPixels(imageData)
+
+    // cast the data from int32 to float32
+    processedTensor = tf.cast(processedTensor, 'float32')
+
+    // normalize the data since it was previously int32
+    processedTensor = tf.div(processedTensor, 255)
+
+    // convert the image to the expected input for this model (4D)
+    processedTensor = processedTensor.reshape([-1, 104, 104, 3])
+
+    return processedTensor
+  }
+
+  // deprocess the image so that it's in the format which we can render
+  deprocessImage(tensor) {
+    // convert the 4D tensor to 3D so that we can render it
+    let deprocessedTensor = tensor.reshape([104, 104, 3])
+
+    return deprocessedTensor
+  }
+
+  // styles an image and draws it to a canvas
+  styleImage () {
     const { imageData, model } = this.state
 
+    // exit if the model has not loaded
     if (model === null) {
       return
     }
 
-    let tfExample = tf.fromPixels(imageData)
-    tfExample = tf.cast(tfExample, 'float32')
-    tfExample = tf.div(tfExample, 255)
+    // preprocess the image so that it's in the format we expect
+    let tfExample = this.preprocessImage(imageData)
 
-    this.renderPrediction(tfExample)
-
-    tfExample = tfExample.reshape([-1, 104, 104, 3])
-
+    // make prediction
     let reconstruction = model.predict(tfExample)
 
-    reconstruction = reconstruction.reshape([104, 104, 3])
+    // deprocess so that we can render it
+    reconstruction = this.deprocessImage(reconstruction)
 
-    this.renderPrediction(reconstruction)
-  }
-
-  renderPrediction(reconstructionImage) {
-    tf.toPixels(reconstructionImage, this.refs.reconstructionCanvas)
+    // draw tensor to canvas
+    tf.toPixels(reconstruction, this.refs.styledCanvas)
   }
 
   render() {
     return (
       <div className="App">
-        <h1>animeGM - styling images as anime</h1>
+        <h1>animeGN - Style images as anime</h1>
+
         <div>
           <div>
             <p>Example Image</p>
             <canvas ref="exampleCanvas" width={104} height={104} />
           </div>
+
           <div>
-            <p>Reconstructed Image</p>
-            <canvas ref="reconstructionCanvas" width={104} height={104} />
+            <p>Styled Image</p>
+            <canvas ref="styledCanvas" width={104} height={104} />
           </div>
         </div>
-        <input type='button' value='reconstruct image!' onClick={this.reconstructImage} />
+
+        <input type='button' value='Style Image!' onClick={this.styleImage} />
       </div>
     );
   }
